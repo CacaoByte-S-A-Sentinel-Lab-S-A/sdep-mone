cc1b0310a92c2f4a4711c11de7460b70a668d68f
mario.arita <jorge.arita@grupopdc.com>
2025-04-18 21:12:28 -0600

    build(update): :art: Update to `Tailwindcss v4` ✅

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
 
-import icon from "astro-icon";
+import react from '@astrojs/react';
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import swup from "@swup/astro";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
-import rehypeSlug from "rehype-slug";
-import rehypeKatex from "rehype-katex";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeKatex from "rehype-katex";
+import rehypeSlug from "rehype-slug";
 import remarkMath from "remark-math";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
@@ -19,9 +20,10 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: YukinaConfig.site,
+
   integrations: [
-    tailwind(),
     svelte(),
+    react(),
     icon(),
     swup({
       theme: false,
@@ -37,6 +39,7 @@ export default defineConfig({
     sitemap(),
     pagefind(),
   ],
+
   markdown: {
     shikiConfig: {
       theme: "github-dark-default",
@@ -52,5 +55,8 @@ export default defineConfig({
         },
       ],
     ],
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
